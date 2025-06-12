f0c6ecab050f9e43577d1ac96a40a633dc7bbab9
Fabian-dev-ux <antagonik.studio@gmail.com>
2025-06-12 12:47:23 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 const nextConfig = {
   output: 'export', // Necesario para generar sitio estático
   trailingSlash: true, // Mejora compatibilidad con hosting estático
@@ -13,4 +17,4 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
